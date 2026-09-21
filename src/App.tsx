7d6e6b1d84376e0b5210d25c37326782c994e1aa
AlexisLeprince <58519818+AlexisLeprince@users.noreply.github.com>
2026-09-21 13:32:38 +0200

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -16,8 +16,16 @@ function App() {
 
   const [error, setError] = useState<string>("");
 
+  const [addInput, setAddInput] = useState<boolean>(false);
+
+  const [listName, setListName] = useState<string>(() => {
+    const savedListName = localStorage.getItem("listName");
+    return savedListName ? JSON.parse(savedListName) : "";
+  });
+
   // Variables
   const content = useRef<HTMLInputElement | null>(null);
+  const listNameInput = useRef<HTMLInputElement | null>(null);
 
   // Functions
   const doneClickHandler = (index: number) => {
@@ -29,6 +37,18 @@ function App() {
   const removeTask = (index: number) => {
     const newTasks = [...tasks];
     newTasks.splice(index, 1);
+    setTasks(newTasks);
+  };
+
+  const removeAllTasks = () => {
+    setTasks([]);
+  };
+
+  const selectAllTasksHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newTasks = tasks.map((task) => ({
+      ...task,
+      done: e.target.checked,
+    }));
     setTasks(newTasks);
   };
 
@@ -47,6 +67,19 @@ function App() {
     setError("");
   };
 
+  const setNewListName = (e: React.SyntheticEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const value = listNameInput.current?.value.trim();
+
+    if (!value) {
+      setError("Veuillez saisir un nom de liste");
+      return;
+    }
+    setListName(value);
+    setError("");
+  };
+
   // Cycle
   useEffect(() => {
     content.current?.focus();
@@ -60,12 +93,47 @@ function App() {
     }
   }, [tasks]);
 
+  useEffect(() => {
+    localStorage.setItem("listName", JSON.stringify(listName));
+  }, [listName]);
+
   // HTML
   return (
     <div className="App">
       <header>
         <span>TO-DO</span>
       </header>
+      {!listName &&
+        (!addInput ? (
+          <button
+            type="button"
+            onClick={() => setAddInput(!addInput)}
+            className="addInputListName"
+          >
+            Ajouter un nom à cette liste
+          </button>
+        ) : (
+          <div>
+            <button
+              type="button"
+              onClick={() => setAddInput(!addInput)}
+              className="addInputListName"
+            >
+              Retirer le nom de cette liste
+            </button>
+            <div className="addListName">
+              <form onSubmit={(event) => setNewListName(event)}>
+                <input
+                  type="text"
+                  placeholder="Nom de la liste"
+                  ref={listNameInput}
+                  onChange={() => setError("")}
+                />
+                <button type="submit">Ajouter</button>
+              </form>
+            </div>
+          </div>
+        ))}
 
       <div className="add">
         <form
@@ -88,6 +156,59 @@ function App() {
           </p>
         )}
       </div>
+      {listName && (
+        <div className="list-name-container">
+          <h2 className="list-name">{listName}</h2>
+          <button type="button" onClick={() => listNameHandler()}>
+            Modifier le nom de la liste
+          </button>
+          <button
+            type="button"
+            className="remove-button"
+            onClick={() => removeListName()}
+          >
+            Supprimer le nom de la liste
+          </button>
+        </div>
+      )}
+      {tasks.length > 0 && (
+        <div>
+          <div className="task-actions">
+            <label>
+              <input
+                type="checkbox"
+                onChange={(e) => selectAllTasksHandler(e)}
+              />
+              Tout sélectionner
+            </label>
+
+            <button
+              type="button"
+              className="clear-tasks"
+              onClick={removeAllTasks}
+              disabled={tasks.length === 0}
+            >
+              <svg
+                xmlns="http://w3.org"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <polyline points="3 6 5 6 21 6"></polyline>
+                <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+                <line x1="10" y1="11" x2="10" y2="17"></line>
+                <line x1="14" y1="11" x2="14" y2="17"></line>
+              </svg>
+              Supprimer toutes les tâches
+            </button>
+          </div>
+        </div>
+      )}
 
       {tasks.map((task, index) => (
         <Task
